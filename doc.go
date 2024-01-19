d9c7b245f1088536ee2bea07932315dba1a1a85c
Justin Simmons <justin.simmons01@protonmail.com>
2024-01-19 10:39:29 -0500

    Add package doc and go mod

---
+++ doc.go
@@ -0,0 +1,8 @@
+// Coinbase Advanced Trade API SDK for Golang.
+// The Advanced Trade API (or Advanced API) supports programmatic trading and order management with a REST API and WebSocket protocol for real-time market data.
+// REST API documentation: https://docs.cloud.coinbase.com/advanced-trade-api/docs/rest-api-overview
+// Web Socket protocol: https://docs.cloud.coinbase.com/advanced-trade-api/docs/ws-overview
+//
+// Coinbase Advanced replaced Coinbase Pro as the "advanced" trading platform.
+// The Coinbase Advanced API lets you manage orders, products, and fees with the new v3 endpoints. It does not duplicate core Coinbase functions for account deposits, withdrawals, and transactions. The Coinbase Advanced REST API supports advanced trading features only.
+package cb
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/justsimmons/go-coinbase
+
+go 1.21
+
+require github.com/google/uuid v1.3.1
