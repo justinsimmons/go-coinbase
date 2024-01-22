c0ee2cd4c748fd256d9aa33fd9db68b10c498714
Justin Simmons <justin.simmons01@protonmail.com>
2024-01-22 13:22:49 -0500

    Comply with GNU licensing guidelines
    Add a copyright notice at the top of each file.
    
    Add a license notice at the top of each file to remove uncertainty
    about whether the license applies to only a portion of the program. As
    advocated by GNU https://www.gnu.org/licenses/gpl-howto.html in their
    documentation.
    
    Add a source link that leads users to an archive of the code inside
    the doc.go file. This is to comply with GNU AGPL.

--- client.go
+++ client.go
@@ -1,3 +1,10 @@
+// Copyright 2024 Justin Simmons.
+//
+// This file is part of go-coinbase.
+// go-coinbase is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+// go-coinbase is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+// You should have received a copy of the GNU Affero General Public License along with go-coinbase. If not, see <https://www.gnu.org/licenses/>.
+
 package coinbase
 
 import "net/http"
--- doc.go
+++ doc.go
@@ -1,4 +1,14 @@
+// Copyright 2024 Justin Simmons.
+//
+// This file is part of go-coinbase.
+// go-coinbase is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+// go-coinbase is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+// You should have received a copy of the GNU Affero General Public License along with go-coinbase. If not, see <https://www.gnu.org/licenses/>.
+
 // Coinbase Advanced Trade API SDK for Golang.
+//
+// Source code available at: https://github.com/justinsimmons/go-coinbase
+//
 // The Advanced Trade API (or Advanced API) supports programmatic trading and order management with a REST API and WebSocket protocol for real-time market data.
 // REST API documentation: https://docs.cloud.coinbase.com/advanced-trade-api/docs/rest-api-overview
 // Web Socket protocol: https://docs.cloud.coinbase.com/advanced-trade-api/docs/ws-overview
